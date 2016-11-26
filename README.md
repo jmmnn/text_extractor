630f06c0acc9c235fb536ad9788303079ae9fa77
jmmnn_1 <jmmnn@yahoo.com>
2016-11-26 03:49:16 +0000

    working version

--- README.md
+++ README.md
@@ -2,6 +2,22 @@
 
 This script takes rich files from a folder e.g. word, pdf, etc. and extracts the text from them using Apache Tika.
 
-The resulting text is stored in various ways:
-- Saved as .txt files for each original file
+The resulting text is saved as .json files for each original file.
 
+Instructions
+-------------
+
+1 - Obtain a new Ubuntu server [e.g. c9.io (free), VirtualBox, AWS, Godaddy cloud, etc.]
+2 - Copy the installer script to the server:
+`$ wget https://raw.githubusercontent.com/jmmnn/text_extractor/master/server_install.py`
+3 - Run the istaller, click yes when necessary:
+`$ python server_install.py`
+At this point you have what you need.
+
+If you want to test it:
+4 - Change directory to text_extractor:
+`$ cd text_extractor`
+5 - Then run:
+`$ python text_extract.py`
+
+To run, just place your files in the "original_files" folder and run the command above again.
--- server_install.py
+++ server_install.py
@@ -1,29 +1,22 @@
 #!/usr/bin/env python
 
-# Setting up the environment
-
+'''
+Setting up your new Ubuntu server
+'''
 import subprocess
 import sys
 import os
 
-#List commands to execute hereself.
+#List commands to execute.
 
 #Server setup
 UPDATE ="sudo apt-get update"
 GIT = "sudo apt-get install git"
 PIP = "sudo apt-get install python-pip"
 JAVA = "sudo apt-get install openjdk-7-jre"
-UNZIP = "sudo apt-get install unzip"
-
-#######  Solr install and start
-#Solr configurations
-collection = "new"
-solr_port = "8080"
-solr_server = "localhost"
-
+CLONE_REPO = "git clone https://github.com/jmmnn/text_extractor.git"
 
 #######  Python stuff
-SOLRPY = "sudo pip install -U solrpy"
 TIKA = "sudo pip install -U tika"
 
 #FIRST list of commands in sequence ## Uncomment these for 1st install
@@ -32,20 +25,8 @@ cmds = [
     GIT,
     PIP,
     JAVA,
-    UNZIP
-    ]
-
-
-#SECOND list of commands in sequence ## Comment Creata_collection after the first install
-cmds2 = [ 
-    SOLRPY,
-    TIKA
-    ]
-    
-    
-#THIRD list of commands in sequence
-cmds3 = [ 
-     
+    TIKA,
+    CLONE_REPO
     ]
 
 dir = os.getcwd()
@@ -55,20 +36,6 @@ print dir
 count=0
 for cmd in cmds:
     count+=1
-    print "Running Command Number %s" % count
+    print "____Running Command Number : ",count , " $" , cmd
     subprocess.call(cmd, shell=True)
-    
-
-###### Iterates over the SECOND list of commands
-count2=0
-for cmd in cmds2:
-    count2+=1
-    print "Running Command Number %s" % count2
-    subprocess.call(cmd, shell=True)
-    
-###### Iterates over the THIRD list of commands
-count3=0
-for cmd in cmds3:
-    count3+=1
-    print "Running Command Number %s" % count3
-    subprocess.call(cmd, shell=True)
+    print "____Finished Running Command: $" , cmd
